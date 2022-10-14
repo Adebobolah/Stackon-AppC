4da6bf1063749629881f6bd0daff138de072fab5
Adebobolah <79697581+Adebobolah@users.noreply.github.com>
2022-10-14 21:15:34 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
-// require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
